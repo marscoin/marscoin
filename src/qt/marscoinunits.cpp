7b084a6ff213aaa0b306dc405484caaafef15c1d
imho <imho@MacBook-Pro-imho.local>
2014-02-10 16:44:16 +0300

    add new

---
+++ src/qt/marscoinunits.cpp
@@ -0,0 +1,181 @@
+#include "marscoinunits.h"
+
+#include <QStringList>
+
+MarscoinUnits::MarscoinUnits(QObject *parent):
+        QAbstractListModel(parent),
+        unitlist(availableUnits())
+{
+}
+
+QList<MarscoinUnits::Unit> MarscoinUnits::availableUnits()
+{
+    QList<MarscoinUnits::Unit> unitlist;
+    unitlist.append(MRS);
+    unitlist.append(mMRS);
+    unitlist.append(uMRS);
+    return unitlist;
+}
+
+bool MarscoinUnits::valid(int unit)
+{
+    switch(unit)
+    {
+    case MRS:
+    case mMRS:
+    case uMRS:
+        return true;
+    default:
+        return false;
+    }
+}
+
+QString MarscoinUnits::name(int unit)
+{
+    switch(unit)
+    {
+    case MRS: return QString("MRS");
+    case mMRS: return QString("mMRS");
+    case uMRS: return QString::fromUtf8("μMRS");
+    default: return QString("???");
+    }
+}
+
+QString MarscoinUnits::description(int unit)
+{
+    switch(unit)
+    {
+    case MRS: return QString("Marscoins");
+    case mMRS: return QString("Milli-Marscoins (1 / 1,000)");
+    case uMRS: return QString("Micro-Marscoins (1 / 1,000,000)");
+    default: return QString("???");
+    }
+}
+
+qint64 MarscoinUnits::factor(int unit)
+{
+    switch(unit)
+    {
+    case MRS:  return 100000000;
+    case mMRS: return 100000;
+    case uMRS: return 100;
+    default:   return 100000000;
+    }
+}
+
+int MarscoinUnits::amountDigits(int unit)
+{
+    switch(unit)
+    {
+    case MRS: return 8; // 21,000,000 (# digits, without commas)
+    case mMRS: return 11; // 21,000,000,000
+    case uMRS: return 14; // 21,000,000,000,000
+    default: return 0;
+    }
+}
+
+int MarscoinUnits::decimals(int unit)
+{
+    switch(unit)
+    {
+    case MRS: return 8;
+    case mMRS: return 5;
+    case uMRS: return 2;
+    default: return 0;
+    }
+}
+
+QString MarscoinUnits::format(int unit, qint64 n, bool fPlus)
+{
+    // Note: not using straight sprintf here because we do NOT want
+    // localized number formatting.
+    if(!valid(unit))
+        return QString(); // Refuse to format invalid unit
+    qint64 coin = factor(unit);
+    int num_decimals = decimals(unit);
+    qint64 n_abs = (n > 0 ? n : -n);
+    qint64 quotient = n_abs / coin;
+    qint64 remainder = n_abs % coin;
+    QString quotient_str = QString::number(quotient);
+    QString remainder_str = QString::number(remainder).rightJustified(num_decimals, '0');
+
+    // Right-trim excess zeros after the decimal point
+    int nTrim = 0;
+    for (int i = remainder_str.size()-1; i>=2 && (remainder_str.at(i) == '0'); --i)
+        ++nTrim;
+    remainder_str.chop(nTrim);
+
+    if (n < 0)
+        quotient_str.insert(0, '-');
+    else if (fPlus && n > 0)
+        quotient_str.insert(0, '+');
+    return quotient_str + QString(".") + remainder_str;
+}
+
+QString MarscoinUnits::formatWithUnit(int unit, qint64 amount, bool plussign)
+{
+    return format(unit, amount, plussign) + QString(" ") + name(unit);
+}
+
+bool MarscoinUnits::parse(int unit, const QString &value, qint64 *val_out)
+{
+    if(!valid(unit) || value.isEmpty())
+        return false; // Refuse to parse invalid unit or empty string
+    int num_decimals = decimals(unit);
+    QStringList parts = value.split(".");
+
+    if(parts.size() > 2)
+    {
+        return false; // More than one dot
+    }
+    QString whole = parts[0];
+    QString decimals;
+
+    if(parts.size() > 1)
+    {
+        decimals = parts[1];
+    }
+    if(decimals.size() > num_decimals)
+    {
+        return false; // Exceeds max precision
+    }
+    bool ok = false;
+    QString str = whole + decimals.leftJustified(num_decimals, '0');
+
+    if(str.size() > 18)
+    {
+        return false; // Longer numbers will exceed 63 bits
+    }
+    qint64 retvalue = str.toLongLong(&ok);
+    if(val_out)
+    {
+        *val_out = retvalue;
+    }
+    return ok;
+}
+
+int MarscoinUnits::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return unitlist.size();
+}
+
+QVariant MarscoinUnits::data(const QModelIndex &index, int role) const
+{
+    int row = index.row();
+    if(row >= 0 && row < unitlist.size())
+    {
+        Unit unit = unitlist.at(row);
+        switch(role)
+        {
+        case Qt::EditRole:
+        case Qt::DisplayRole:
+            return QVariant(name(unit));
+        case Qt::ToolTipRole:
+            return QVariant(description(unit));
+        case UnitRole:
+            return QVariant(static_cast<int>(unit));
+        }
+    }
+    return QVariant();
+}
