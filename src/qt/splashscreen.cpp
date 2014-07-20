df425a80295b97f8d3d3d272912d591f571598c9
marscoin <info@marscoin.org>
2014-07-20 18:51:52 -0400

    fixed splashscreen text location

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -11,7 +11,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
 {
     // set reference point, paddings
     int paddingLeftCol2         = 230;
-    int paddingTopCol2          = 376;
+    int paddingTopCol2          = 316;
     int line1 = 0;
     int line2 = 13;
     int line3 = 26;
@@ -21,8 +21,8 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     // define text to place
     QString titleText       = QString(QApplication::applicationName()).replace(QString("-testnet"), QString(""), Qt::CaseSensitive); // cut of testnet, place it as single object further down
     QString versionText     = QString("Version %1 ").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Marscoin developers"));
-    QString copyrightText2   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Marscoin developers"));
+    QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("Bitcoin / Litecoin"));
+    QString copyrightText2   = QChar(0xA9)+QString(" 2014-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("Marscoin developers"));
 
     QString font            = "Arial";
 
@@ -36,13 +36,13 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     }
 
     QPainter pixPaint(&newPixmap);
-    pixPaint.setPen(QColor(70,70,70));
+    pixPaint.setPen(QColor(255,255,255));
 
-    pixPaint.setFont(QFont(font, 9*fontFactor));
+    pixPaint.setFont(QFont(font, 10*fontFactor));
     pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line3,versionText);
 
     // draw copyright stuff
-    pixPaint.setFont(QFont(font, 9*fontFactor));
+    pixPaint.setFont(QFont(font, 10*fontFactor));
     pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line1,copyrightText1);
     pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line2,copyrightText2);
 
