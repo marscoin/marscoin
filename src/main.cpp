90b42772f2d0f3bdb2b5399bd71b4183b42fd077
Lennart Lopin <novalis78@gmail.com>
2018-05-06 23:16:47 -0400

    incremental update
    from 0.8.6.2 to 0.8.7.1 (upstream)

--- src/main.cpp
+++ src/main.cpp
@@ -938,6 +938,27 @@ int CMerkleTx::GetDepthInMainChain(CBlockIndex* &pindexRet) const
     return nResult;
 }
 
+double ConvertBitsToDouble(unsigned int nBits){
+    int nShift = (nBits >> 24) & 0xff;
+
+    double dDiff =
+        (double)0x0000ffff / (double)(nBits & 0x00ffffff);
+
+    while (nShift < 29)
+    {
+        dDiff *= 256.0;
+        nShift++;
+    }
+    while (nShift > 29)
+    {
+        dDiff /= 256.0;
+        nShift--;
+    }
+
+    return dDiff;
+}
+
+
 int CMerkleTx::GetBlocksToMaturity() const
 {
     if (!IsCoinBase())
