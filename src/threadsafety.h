d6f3619c94a237f2e7d5a27b76cb71e3638752e0
Lennart Lopin <novalis78@gmail.com>
2018-05-20 16:37:28 -0400

    Incremental update
    from 0.8.7.5 to 0.9.2 (upstream)

--- src/threadsafety.h
+++ src/threadsafety.h
@@ -1,7 +1,8 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2019 The Bitcoin/Marscoin developers
+// Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef MARSCOIN_THREADSAFETY_H
 #define MARSCOIN_THREADSAFETY_H
 
