a95e98c5e3c1db51422b6dbfeaa6bc6ddff87c31
Lennart Lopin <novalis78@gmail.com>
2021-07-16 21:43:35 -0400

    Qt Makefile

--- contrib/verify-commits/pre-push-hook.sh
+++ contrib/verify-commits/pre-push-hook.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
-# 
-# File:   pre-push-hook.sh
-# Author: novalis78
-#
-# Created on Jul 16, 2021, 7:42:08 PM
-#
+#!/bin/bash
+if ! [[ "$2" =~ [git@]?[www.]?github.com[:|/]bitcoin/bitcoin[.git]? ]]; then
+    exit 0
+fi
+
+while read LINE; do
+    set -- A $LINE
+    if [ "$4" != "refs/heads/master" ]; then
+        continue
+    fi
+    if ! ./contrib/verify-commits/verify-commits.sh $3 > /dev/null 2>&1; then
+        echo "ERROR: A commit is not signed, can't push"
+        ./contrib/verify-commits/verify-commits.sh
+        exit 1
+    fi
+done < /dev/stdin
